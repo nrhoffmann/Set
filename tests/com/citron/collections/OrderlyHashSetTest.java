7ea2d6f806569f70afcc705c8e45eba9456ad7c0
JudahSchwartz <judahsch@gmail.com>
2017-11-10 09:37:26 -0500

    Wrote a few tests

--- tests/com/citron/collections/OrderlyHashSetTest.java
+++ tests/com/citron/collections/OrderlyHashSetTest.java
@@ -2,12 +2,33 @@ package com.citron.collections;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.*;
+
 public class OrderlyHashSetTest {
+
 
     @Test
     public void iterationTest()
     {
         OrderlyHashSet<String> testSet = new OrderlyHashSet<>();
+        for(int i = 'A'; i < 'J'; i++)
+        {
+            testSet.add("" + (char)i);
+        }
+        for(String s: testSet)
+        {
+            System.out.println(s);
+        }
+    }
+    @Test public void containsTest()
+    {
+        OrderlyHashSet<String> testSet = new OrderlyHashSet<>();
+        for(int i = 'A'; i < 'J'; i++)
+        {
+            testSet.add("" + (char)i);
+        }
+       assertTrue(testSet.contains("A")&&testSet.contains("B")&&testSet.contains("I"));
+        assertFalse(testSet.contains("S"));
     }
 
 }
